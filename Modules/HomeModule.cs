745c952a373733399d5533e476bc385740736795
James Howard and Joel Delight <james+joel@>
2016-07-12 16:49:28 -0700

    create animal pages, and leave a bug on add animal.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,9 +13,32 @@ namespace AnimalShelter
         return View["index.cshtml", allSpecies];
       };
       Get["species/new"] = _ => View["species_form.cshtml"];
+
       Post["species/new"] = _ => {
         Species newSpecies = new Species(Request.Form["species-name"]);
         newSpecies.Save();
+        List<Species> allSpecies = Species.GetAll();
+        return View["index.cshtml", allSpecies];
+      };
+
+      Get["species/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedSpecies = Species.Find(parameters.id);
+        var speciesAnimals = selectedSpecies.GetAnimal();
+        model.Add("species", selectedSpecies);
+        model.Add("animals", speciesAnimals);
+        return View["species.cshtml", model];
+      };
+
+      Get["animal/new/{id}"] = parameters => {
+        Species selectedSpecies = Species.Find(parameters.id);
+
+        return View["animal_form.cshtml", selectedSpecies];
+      };
+
+      Post["animal/new/{id}"] = parameters => {
+        Animal newAnimal = new Animal(Request.Form["animal-name"], Request.Form["animal-breed"], Request.Form["animal-gender"], Request.Form["animal-age"], Request.Form["animal-species"]);
+        newAnimal.Save();
         List<Species> allSpecies = Species.GetAll();
         return View["index.cshtml", allSpecies];
       };
